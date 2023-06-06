83c0d249e81b1b743ef337a3f7ddabfa456f9e3c
kapilsarma2002 <sarmakapil1818@gmail.com>
2023-06-06 21:33:37 +0530

    persistant-data

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import "./index.css";
 import Titlebar from "./components/Titlebar";
 import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
+import "./index.css";
 
 function App() {
 
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,47 +1,61 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { Component } from "react";
+import { EditorState, convertFromRaw, convertToRaw, ContentState, RawDraftContentState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
-const TextEditor = () => {
-  const content = window.localStorage.getItem("content");
-  let temp = EditorState.createEmpty();
-  if (content) {
-    temp = EditorState.createWithContent(convertFromRaw(JSON.parse(content)));
-  } 
-  
-  const [editorState, setEditorState] = useState(temp);
+class TextEditor extends Component<{}, {editorState: EditorState}> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editorState: EditorState.createEmpty()
+    };
+  }
 
-  const onEditorStateChange = (editorState) => {
-    setEditorState(editorState);
-    const state = editorState.getCurrentContent();
-    saveContent(state);
+  onChange = (editorState: EditorState) => {
+    const contentState = editorState.getCurrentContent();
+    // console.log("content state", convertToRaw(contentState));
+    this.saveContent(contentState);
+    this.setState({
+      editorState,
+    });
   };
 
-  const saveContent = (content) => {
+  componentDidMount(): void {
+    const content = window.localStorage.getItem("content");
+
+    if (content) {
+      this.setState({
+        editorState: EditorState.createWithContent(
+          convertFromRaw(JSON.parse(content))
+        ),
+      });
+    } else {
+      this.setState({ editorState: EditorState.createEmpty() });
+    }
+    console.log('state is : ')
+    console.log(this.state);
+  }
+
+  saveContent = (content) => {
     window.localStorage.setItem(
       "content",
       JSON.stringify(convertToRaw(content))
     );
   };
 
-  const editorRef = useRef(null);
-  useEffect(() => {
-    editorRef.current.focusEditor();
-  });
-
-  return (
-    <div className="white text-md fixed h-full w-11/12 bg-[#dde0e4] text-center">
-      <Editor
-        ref={editorRef}
-        editorState={editorState}
-        onEditorStateChange={onEditorStateChange}
-        wrapperClassName="h-full"
-        toolbarClassName="mt-16 flex fixed top-0 z-50 !justify-center max-auto w-full"
-        editorClassName="bg-white shadow-lg max-w-4xl mx-auto border-2 border-solid border-blue m-4 h-48 text-md p-4 px-8 pt-24"
-      />
-    </div>
-  );
-};
+  render() {
+    return (
+      <div className="white text-md fixed h-full w-11/12 bg-[#dde0e4] text-center">
+        <Editor
+          editorState={this.state.editorState}
+          onEditorStateChange={this.onChange}
+          wrapperClassName="h-full"
+          toolbarClassName="mt-16 flex fixed top-0 z-50 !justify-center max-auto w-full"
+          editorClassName="bg-white shadow-lg max-w-4xl mx-auto border-2 border-solid border-blue m-4 h-48 text-md p-4 px-8 pt-24"
+        />
+      </div>
+    );
+  }
+}
 
 export default TextEditor;
